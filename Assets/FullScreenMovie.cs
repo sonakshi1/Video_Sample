ccfbd136bee3105ffc122959bbd06daae83b54fc
sonakshi1 <sona.gargmt5@gmail.com>
2016-02-01 00:38:18 +0530

    First commit

---
+++ Assets/FullScreenMovie.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FullScreenMovie : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Handheld.PlayFullScreenMovie("Jugnoo2.mp4");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Vuforia/Scripts/Play1.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Play1 : MonoBehaviour {
+
+	public MovieTexture movTexture;
+	// Use this for initialization
+	void Start () {
+		GetComponent<Renderer>().material.mainTexture = movTexture;
+		movTexture.Play();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
